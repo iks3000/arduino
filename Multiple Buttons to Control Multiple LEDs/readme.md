5b0d7a8043ffabaf41dc913d7f9988f5efeecf24
Stanislav <khomyn@gmail.com>
2022-02-11 13:53:48 -0800

    update proj multiple btns control

---
+++ Multiple Buttons to Control Multiple LEDs/readme.md
@@ -0,0 +1,8 @@
+# Using Multiple Buttons to Control Multiple LEDs
+
+In this project you will create a circuit with 4 buttons and 4 LEDs as shown in Connection diagram. When controlling lights, typically you are used to associating a switch with a given light or set of lights—if you change the position of a switch, the same lights always turn off or on.
+
+Connection Diagram
+![Connection Diagram](breadboard_connection_diagram.png)
+
+Code placed in ```sketch_feb10a.ino```
--- Multiple Buttons to Control Multiple LEDs/sketch_feb10a.ino
+++ Multiple Buttons to Control Multiple LEDs/sketch_feb10a.ino
@@ -9,55 +9,55 @@
 #define LED_PIN_4 11
 
 void setup() {
-  //Serial.begin(9600);
-  pinMode(BUTTON_PIN_1, INPUT_PULLUP);
-  pinMode(BUTTON_PIN_2, INPUT_PULLUP);
-  pinMode(BUTTON_PIN_3, INPUT_PULLUP);
-  pinMode(BUTTON_PIN_4, INPUT_PULLUP);
-  
-  pinMode(LED_PIN_1, OUTPUT);
-  pinMode(LED_PIN_2, OUTPUT);
-  pinMode(LED_PIN_3, OUTPUT);
-  pinMode(LED_PIN_4, OUTPUT);
+    // Serial.begin(9600);
+    pinMode(BUTTON_PIN_1, INPUT_PULLUP);
+    pinMode(BUTTON_PIN_2, INPUT_PULLUP);
+    pinMode(BUTTON_PIN_3, INPUT_PULLUP);
+    pinMode(BUTTON_PIN_4, INPUT_PULLUP);
+
+    pinMode(LED_PIN_1, OUTPUT);
+    pinMode(LED_PIN_2, OUTPUT);
+    pinMode(LED_PIN_3, OUTPUT);
+    pinMode(LED_PIN_4, OUTPUT);
 }
 
 void loop() {
-  byte buttonState1 = digitalRead(BUTTON_PIN_1);
-  byte buttonState2 = digitalRead(BUTTON_PIN_2);
-  byte buttonState3 = digitalRead(BUTTON_PIN_3);
-  byte buttonState4 = digitalRead(BUTTON_PIN_4);
-  
-  if (buttonState1 == LOW) {
-      //Serial.println("Button-1 is pressed");
-      digitalWrite(LED_PIN_1, HIGH);
-  } else {
-      //Serial.println("Button-1 is not pressed");
-      digitalWrite(LED_PIN_1, LOW);
-  }
+    byte buttonState1 = digitalRead(BUTTON_PIN_1);
+    byte buttonState2 = digitalRead(BUTTON_PIN_2);
+    byte buttonState3 = digitalRead(BUTTON_PIN_3);
+    byte buttonState4 = digitalRead(BUTTON_PIN_4);
 
-  if (buttonState2 == LOW) {
-      //Serial.println("Button-2 is pressed");
-      digitalWrite(LED_PIN_2, HIGH);
-  } else {
-      //Serial.println("Button-2 is not pressed");
-      digitalWrite(LED_PIN_2, LOW);
-  }
+    if (buttonState1 == LOW) {
+        // Serial.println("Button-1 is pressed");
+        digitalWrite(LED_PIN_1, HIGH);
+    } else {
+        // Serial.println("Button-1 is not pressed");
+        digitalWrite(LED_PIN_1, LOW);
+    }
 
-  if (buttonState3 == LOW) {
-      //Serial.println("Button-3 is pressed");
-      digitalWrite(LED_PIN_3, HIGH);
-  } else {
-      //Serial.println("Button-3 is not pressed");
-      digitalWrite(LED_PIN_3, LOW);
-  }
+    if (buttonState2 == LOW) {
+        // Serial.println("Button-2 is pressed");
+        digitalWrite(LED_PIN_2, HIGH);
+    } else {
+        // Serial.println("Button-2 is not pressed");
+        digitalWrite(LED_PIN_2, LOW);
+    }
 
-  if (buttonState4 == LOW) {
-      //Serial.println("Button-4 is pressed");
-      digitalWrite(LED_PIN_4, HIGH);
-  } else {
-      //Serial.println("Button-3 is not pressed");
-      digitalWrite(LED_PIN_4, LOW);
-  }
-  
-  delay(100);
+    if (buttonState3 == LOW) {
+        // Serial.println("Button-3 is pressed");
+        digitalWrite(LED_PIN_3, HIGH);
+    } else {
+        // Serial.println("Button-3 is not pressed");
+        digitalWrite(LED_PIN_3, LOW);
+    }
+
+    if (buttonState4 == LOW) {
+        // Serial.println("Button-4 is pressed");
+        digitalWrite(LED_PIN_4, HIGH);
+    } else {
+        // Serial.println("Button-3 is not pressed");
+        digitalWrite(LED_PIN_4, LOW);
+    }
+
+    delay(100);
 }
